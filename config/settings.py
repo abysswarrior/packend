c00e1cdfe8bb34bb8d224d67d723b541badc3368
promethe <mehran.safaripour@gmail.com>
2022-11-17 13:00:42 +0330

    feat: add expatica  app

--- config/settings.py
+++ config/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'home.apps.HomeConfig',
     'relocateme.apps.RelocatemeConfig',
+    'expatica.apps.ExpaticaConfig',
 ]
 
 MIDDLEWARE = [
