1aafc6986da9638f46acb07ed5f930462ad384f2
abysswarrior <mehran.safaripour@gmail.com>
2022-12-03 18:19:58 +0330

    fix(relocateme): fix remote job crawl problem
    some items don't have location (remote jobs) and also relocation package info
    that cause this problem

--- relocateme/crawler.py
+++ relocateme/crawler.py
@@ -8,13 +8,23 @@ def relocatme_crawler(page):
     all_jobs = []
     for job in jobs:
         job_name = job.find(class_="job__title").get_text()
-        job_title, job_location = job_name.strip('\n').split('\n')
+        job_content = job_name.strip('\n').split('\n')
+        job_title = job_content[0]
+        try:
+            job_location = job_content[1]
+        except:
+            job_location = 'Relocation or remote'
         job_preview = job.find(class_="job__preview").get_text()
         job_info = job.find(class_="job__info")
         job_company = job_info.find(class_="job__company").get_text().strip('\n')
-        relocation_info = job_info.find(class_='relocation-package-wrapper').find_all('div')
-        relocation_type = relocation_info[0].get_text().strip('\n')
-        relocation_details = relocation_info[1].get_text().strip('\n').split('\n\n\n\n')
+        try:
+            relocation_info = job_info.find(class_='relocation-package-wrapper').find_all('div')
+            relocation_type = relocation_info[0].get_text().strip('\n')
+            relocation_details = relocation_info[1].get_text().strip('\n').split('\n\n\n\n')
+        except:
+            relocation_type = 'Advanced relocation package'
+            relocation_details = 'Unknown'
+
         job_tags = job.find(class_="job__tags_wrapper").get_text().strip('\n').split('\n')
         link = 'https://relocate.me/' + job.find(class_="job__title").find('a').attrs['href']
         try:
